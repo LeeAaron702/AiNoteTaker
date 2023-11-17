1ec595193047e34e658f63b271c480b114109edf
Lee Seaver <lee.seaver@gmail.com>
2023-11-17 12:35:15 -0800

    Note setup

--- ai-note-app/src/app/notes/NavBar.tsx
+++ ai-note-app/src/app/notes/NavBar.tsx
@@ -1,11 +1,16 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import AddNoteDialog from "@/components/AddNoteDialog";
 
 export default function NavBar() {
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+
   return (
     <>
       <div className="p-4 shadow">
@@ -20,13 +25,14 @@ export default function NavBar() {
               elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } }
             }}
             />
-            <Button>
+            <Button onClick={() => setShowAddNoteDialog(true)}>
               <Plus size={20} className="mr-2" />
               Add Note
             </Button>
           </div>
         </div>
       </div>
+      <AddNoteDialog open={showAddNoteDialog} setOpen={setShowAddNoteDialog} />
     </>
   );
 }
